bea7fcfe1d054e2eaf008a97a353170c88d26afd
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-10-17 12:18:38 -0400

    imported event router file to server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ dotenv.config();
 import connectDB from './db/connect.js';
 
 import authRouter from './routes/authRoutes.js';
+import eventRouter from './routes/eventRoutes.js';
 
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
@@ -13,6 +14,7 @@ import errorHandlerMiddleware from './middleware/error-handler.js';
 app.use(express.json());
 
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/events', eventRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
