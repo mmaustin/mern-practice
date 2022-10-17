32f2754d90db06728374a8fc9f0d7d51ae9666d7
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-10-17 14:08:24 -0400

    created user model in user js file

---
+++ models/User.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: [true, 'Please provide name'],
+      minlength: 3,
+      maxlength: 20,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'Please provide email'],
+      /*validate: {
+        validator: validator.isEmail,
+        message: 'Please provide a valid email',
+      },*/
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please provide password'],
+      minlength: 6,
+      select: false,
+    }
+})
+
+export default mongoose.model('User', UserSchema);
