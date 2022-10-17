4377488aefbae53c9dc905a279e010cdce5bc41a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-10-17 09:39:55 -0400

    fixed issue with gitignore not ignoring .env file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 import dotenv from 'dotenv';
 dotenv.config();
 
-const port = 5001;
+const port = process.env.PORT || 5001
 
 const requestTime = (req, res, next) => {
     req.requestTime = Date.now()
