088e9e3d813aa3de5bb2272530472584c557a135
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-10-17 14:27:08 -0400

    installed validator package and set up try/catch block in register controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,13 @@
-
+import User from '../models/User.js'
 
 
 const register = async (req, res) => {
-    res.status(200).json({msg: 'register user'});
+    try {
+        const user = User.create(req.body)
+        res.status(201).json({user});
+    } catch (error) {
+        res.status(500).json({msg: 'there was an erro'})
+    }
 }
 
 const login = async (req, res) => {
