e1270d6ea0c8a05c008c200761cf0b8658fe35df
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-10-18 09:37:49 -0400

    added a link from the landing page to the register page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,15 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Landing = () => {
-    const clicked = () => {
-        console.log('you clicked the button')
-    }
 
   return (
     <>
         <h4>Virginia Place Happenings!</h4>
         <h3>Having a party or shower; need a sitter?  Tell your neighbors about it!</h3>
-        <button type='button' onClick={clicked}>Register/Login</button>
+        <Link to='/register'>Login/Register</Link>
     </>
   )
 }
