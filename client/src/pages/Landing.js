9f4ef300404b1d9b28d788b49296c4376ecae0da
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-10-26 16:21:42 -0400

    imported react responsive and tested use media query in shared layout component

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 /*import styled from 'styled-components'
 const Button = styled.button`
     background: red;
 `*/
 
-const Landing = () => {
+const Landing = () => { 
 
   return (
     <>
-        <h4>Virginia Place Happenings!</h4>
-        <h3>Having a party or shower; need a sitter?  Tell your neighbors about it!</h3>
-        <Link to='/register'>Login/Register</Link>
+      <h4>Virginia Place Happenings!</h4>
+      <h3>Having a party or shower; need a sitter?  Tell your neighbors about it!</h3>
+      <Link to='/register'>Login/Register</Link>
     </>
   )
 }
--- client/src/pages/dashboard/SharedLayout.js
+++ client/src/pages/dashboard/SharedLayout.js
@@ -1,18 +1,24 @@
-import {Outlet, Link} from 'react-router-dom'
+import {Outlet} from 'react-router-dom';
+import {useMediaQuery} from 'react-responsive';
+import styled from 'styled-components';
 
 const SharedLayout = () => {
 
+  const isTesting = useMediaQuery({
+    query: '(min-width: 992px)'
+  })
 
   return (
     <>
-        <nav>
-          <Link to='/'>All Messages</Link>
+        <main>
+          {isTesting ? <div>Big SideBar</div> : <div>Small Side Bar</div>}
           <div>
-            
+            <div>This Will Be A NavBar Component</div>
+            <div>
+              <Outlet/>
+            </div>
           </div>
-          <Link to='add-message'>Add Message</Link>
-        </nav>
-        <Outlet/>
+        </main>
     </>
   )
 }
