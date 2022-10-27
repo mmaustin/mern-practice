5c6e5a44f3c45bcbec6ef5f03209a73040a9064f
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-10-27 10:37:05 -0400

    implemented logout user capability in navbar component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,25 @@
-import { useAppContext } from "../context/appContext"
-
+import { useAppContext } from "../context/appContext";
+import styled from 'styled-components';
 
 const Navbar = () => {
     const {logoutUser} = useAppContext();
   return (
-    <div>
+    <Wrapper>
         <h3>Navbar</h3>
         <h3>Logo Here</h3>
-        <button type="button" onClick={()=> console.log('logout user')}>Logout</button>
-    </div>
+        <button type="button" onClick={logoutUser}>Logout</button>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+    width: 100%;
+    height: 200px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-flow: wrap;
+    border-bottom: 1px solid blue;
+`
+
 export default Navbar
