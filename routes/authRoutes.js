33853b2d98a8f7474bf6d42359aa727dc4a2419b
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-10-31 09:53:19 -0400

    created auth middleware

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express'
 const router = express.Router();
 
+
 import { register, login } from '../controllers/authController.js';
+import authenticateUser from '../middleware/auth.js'
 
 router.route('/register').post(register);
 router.route('/login').post(login);
