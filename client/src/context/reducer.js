13254b2b2912a36aae1c5fc12deed17e1884e3c4
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-11-02 12:20:57 -0400

    added update user functionality to reducer

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -7,7 +7,10 @@ import {
     LOGIN_USER_BEGIN,
     LOGIN_USER_SUCCESS,
     LOGIN_USER_ERROR,
-    LOGOUT_USER    
+    LOGOUT_USER,
+    UPDATE_USER_BEGIN,
+    UPDATE_USER_SUCCESS,
+    UPDATE_USER_ERROR,       
 } from './actions'
 import { initialState } from './appContext'
 
@@ -69,6 +72,30 @@ const reducer = (state, action) => {
     if(action.type === LOGOUT_USER){
       return {...initialState, user: null, token: null}
     }
+
+    if (action.type === UPDATE_USER_BEGIN) {
+      return { ...state, isLoading: true }
+    }
+    if (action.type === UPDATE_USER_SUCCESS) {
+      return {
+        ...state,
+        isLoading: false,
+        token: action.payload.token,
+        user: action.payload.user,
+        showAlert: true,
+        alertType: 'success',
+        alertText: 'User Profile Updated!',
+      }
+    }
+    if (action.type === UPDATE_USER_ERROR) {
+      return {
+        ...state,
+        isLoading: false,
+        showAlert: true,
+        alertType: 'danger',
+        alertText: action.payload.msg,
+      }
+    }    
 
     throw new Error(`no such action : ${action.type}`)   
 }
