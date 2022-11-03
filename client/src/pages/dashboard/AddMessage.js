6b44c4751ada9c45cf656f3590a93b503e4ee0a8
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-11-03 13:51:21 -0400

    continued testing of add message/event component

--- client/src/pages/dashboard/AddMessage.js
+++ client/src/pages/dashboard/AddMessage.js
@@ -1,44 +1,60 @@
 import { useAppContext } from "../../context/appContext";
 import {FormRow, Alert} from '../../components';
-import { useState } from "react";
+//import { useState } from "react";
 
 
 const AddMessage = () => {
   const {isEditing, showAlert, displayAlert, organizer, description, eventType, eventTypeOptions} = useAppContext();
 
-  const [organize, setOrganizer] = useState(organizer);
-  const [describe, setDescription] = useState(description);
+  // const [organize, setOrganizer] = useState(organizer);
+  // const [describe, setDescription] = useState(description);
 
-  const onSetOrganizer = e => setOrganizer(e.target.value);
-  const onSetDescription = e => setDescription(e.target.value);
+  // const onSetOrganizer = e => setOrganizer(e.target.value);
+  // const onSetDescription = e => setDescription(e.target.value);
 
-  const submitForm = e => {
-    e.preventDefault()
-    console.log(`${organize}: ${describe}`)
+  // const submitForm = e => {
+  //   e.preventDefault()
+  //   console.log(`${organize}: ${describe}`)
+  // }
+
+  const handleEventInput = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(`${name}: ${value}`);
   }
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!organizer || !description){
+      displayAlert();
+      return
+    }
+    console.log('create event');
+  }
 
   return (
     <div>
-      <form onSubmit={submitForm}>
+      <form >
         {isEditing ? 'edit event' : 'add event'}
         {showAlert && <Alert/>}
         <div>
           <FormRow
             type="text"
             name="organizer"
-            value={organize}
-            handleChange={onSetOrganizer}
+            value={organizer}
+            handleChange={handleEventInput}
           />
           <label htmlFor="description">Description</label>
           <textarea
             id="description"
             name="description"
-            value={describe}
-            onChange={onSetDescription}
+            value={description}
+            onChange={handleEventInput}
           ></textarea>
+          <div>
+            <button type="submit" onClick={handleSubmit}>submit form</button>
+          </div>
         </div>
-        <button type="submit" >submit form</button>
       </form>
     </div>
   )
