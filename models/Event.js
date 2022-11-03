45bdd06dcc9814073af06fb9d73e6407dbbfc652
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-11-03 10:31:59 -0400

    created event schema and event model

---
+++ models/Event.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const EventSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: [true, 'Please provide organizer\'s name'],
+      maxlength: 20,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, 'Please provide a description of the event'],
+      maxlength: 200,
+      trim: true,
+    },
+    eventType: {
+        type: String,
+        enum: ['sitter', 'celebration', 'tutoring', 'other'],
+        default: 'other'
+    },
+    createdBy: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'Please provide user'],
+      }, 
+    },
+    {timestamps: true}
+)
+
+export default mongoose.model('Event', EventSchema);
