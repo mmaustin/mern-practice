fc0f24ce85544a3be1308ab58a4c682c2eb423b6
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-11-03 13:19:37 -0400

    tried an alternative method for submitting formrow with textarea

--- client/src/pages/dashboard/AddMessage.js
+++ client/src/pages/dashboard/AddMessage.js
@@ -1,28 +1,44 @@
 import { useAppContext } from "../../context/appContext";
 import {FormRow, Alert} from '../../components';
+import { useState } from "react";
+
 
 const AddMessage = () => {
   const {isEditing, showAlert, displayAlert, organizer, description, eventType, eventTypeOptions} = useAppContext();
 
+  const [organize, setOrganizer] = useState(organizer);
+  const [describe, setDescription] = useState(description);
+
+  const onSetOrganizer = e => setOrganizer(e.target.value);
+  const onSetDescription = e => setDescription(e.target.value);
+
+  const submitForm = e => {
+    e.preventDefault()
+    console.log(`${organize}: ${describe}`)
+  }
+
+
   return (
     <div>
-      <form>
+      <form onSubmit={submitForm}>
         {isEditing ? 'edit event' : 'add event'}
         {showAlert && <Alert/>}
         <div>
           <FormRow
             type="text"
             name="organizer"
-            value={organizer}
-            //handleChange
+            value={organize}
+            handleChange={onSetOrganizer}
           />
           <label htmlFor="description">Description</label>
           <textarea
             id="description"
             name="description"
-            value={description}
+            value={describe}
+            onChange={onSetDescription}
           ></textarea>
         </div>
+        <button type="submit" >submit form</button>
       </form>
     </div>
   )
