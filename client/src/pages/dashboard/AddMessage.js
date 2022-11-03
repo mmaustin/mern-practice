85590f8045cbbdf30d36a94aef3caa588c539e14
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-11-03 12:52:36 -0400

    added form row and textarea properties to return form element

--- client/src/pages/dashboard/AddMessage.js
+++ client/src/pages/dashboard/AddMessage.js
@@ -1,10 +1,30 @@
+import { useAppContext } from "../../context/appContext";
+import {FormRow, Alert} from '../../components';
 
 const AddMessage = () => {
+  const {isEditing, showAlert, displayAlert, organizer, description, eventType, eventTypeOptions} = useAppContext();
+
   return (
-    <>
-      <h3>add a message</h3>
-      <button type="button">Button Does Nothing!</button>
-    </>
+    <div>
+      <form>
+        {isEditing ? 'edit event' : 'add event'}
+        {showAlert && <Alert/>}
+        <div>
+          <FormRow
+            type="text"
+            name="organizer"
+            value={organizer}
+            //handleChange
+          />
+          <label htmlFor="description">Description</label>
+          <textarea
+            id="description"
+            name="description"
+            value={description}
+          ></textarea>
+        </div>
+      </form>
+    </div>
   )
 }
 
