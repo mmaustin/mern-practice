1df2a274ebbadcd46fb2dbb4421c1925d33f8950
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-11-08 11:06:56 -0500

    set up header and footer, with edit and delete shells, in event component

--- client/src/components/Event.js
+++ client/src/components/Event.js
@@ -1,15 +1,32 @@
 import moment from 'moment';
+import {Link} from 'react-router-dom';
+import { useAppContext } from '../context/appContext';
+import EventInfo from './EventInfo';
 
 
-const Event = ({description, createdAt}) => {
+const Event = ({description, createdAt, _id, organizer, eventType}) => {
   let date = moment(createdAt);
   date = date.format('MMMM Do, YYYY');
 
+  const {setEditEvent, deleteEvent} = useAppContext();
+
   return (
-    <>
-      <h3>{description}</h3>
-      <h4>{date}</h4>
-    </>
+    <div>
+      <header>
+        <div>
+          <h3>
+            <p>{organizer}</p>
+            <p>{description}</p>
+          </h3>
+        </div>
+      </header>
+      <footer>
+        <div>
+          <Link to='/add-message' onClick={()=> setEditEvent(_id)}>Edit Event</Link>
+          <button type='button' onClick={()=> deleteEvent(_id)}>Delete Event</button>
+        </div>
+      </footer>
+    </div>
   )
 }
 export default Event
