28559f789b7f35bda306d65973fe011babdfddb0
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-04-13 11:07:16 -0400

    retrive token from req.cookies in auth middleware, commented out doing the same with authorization header

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,11 +3,18 @@ import { UnAuthenticatedError } from '../errors/index.js'
 
 UnAuthenticatedError
 const auth = async (req, res, next) => {
-  const authHeader = req.headers.authorization
-  if (!authHeader || !authHeader.startsWith('Bearer')) {
-    throw new UnAuthenticatedError('Authentication Invalid')
+  const token = req.cookies.token;
+  if(!token){
+    throw new UnAuthenticatedError('Invalid Authorization');
   }
-  const token = authHeader.split(' ')[1]
+
+  //Commented out auth with authorization header
+  // const authHeader = req.headers.authorization
+  // if (!authHeader || !authHeader.startsWith('Bearer')) {
+  //   throw new UnAuthenticatedError('Authentication Invalid')
+  // }
+  // const token = authHeader.split(' ')[1]
+
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET)
     req.user = { userId: payload.userId }
