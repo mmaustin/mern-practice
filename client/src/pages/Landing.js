1d8987e63098ddc16ea3e19854d817cd9fada2c6
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-04-16 12:23:12 -0400

    redirect user to protected routes if logged in and attempted to go to the landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
+import { useAppContext } from '../context/appContext';
 
 /*import styled from 'styled-components'
 const Button = styled.button`
@@ -7,13 +8,16 @@ const Button = styled.button`
 `*/
 
 const Landing = () => { 
-
+  const {user} = useAppContext()
   return (
+    <React.Fragment>
+      {user && <Navigate to='/'/>}
     <>
       <h4>Virginia Place Happenings!</h4>
       <h3>Having a party or shower; need a sitter?  Tell your neighbors about it!</h3>
       <Link to='/register'>Login/Register</Link>
     </>
+    </React.Fragment>
   )
 }
 
