96886cc2737572e229d3e9befa8d81ea7b0568b7
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-04-16 12:34:29 -0400

    set up logout user server functionality to remove cookie on logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,13 +2,15 @@ import express from 'express'
 const router = express.Router();
 
 
-import { register, login, updateUser, getCurrentUser } from '../controllers/authController.js';
+import { register, login, updateUser, getCurrentUser, logout } from '../controllers/authController.js';
 import authenticateUser from '../middleware/auth.js'
 
 router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/updateUser').patch(authenticateUser, updateUser);
 router.route('/getCurrentUser').get(authenticateUser, getCurrentUser);
+router.route('/logout').get(logout);
+
 
 export default router;
 
